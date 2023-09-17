02f4d41f1add96410328575cf54f7f20eea5143d
Naftuli Kay <me@naftuli.wtf>
2023-09-17 15:01:57 -0700

    fix merge typo

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ locals{
   domain_name_short = split(".", var.domain_name)[0]
   ecr_us_east_1 = "${data.aws_caller_identity.default.account_id}.dkr.ecr.us-east-1.amazonaws.com"
 
-  zone_id_map = merge({ var.domain_name: var.zone_id }, var.additional_domain_names)
+  zone_id_map = merge({ (var.domain_name): (var.zone_id) }, var.additional_domain_names)
 }
