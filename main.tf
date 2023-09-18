69f498b1c2ad8327f59565fadc86a7219385b1fa
Naftuli Kay <me@naftuli.wtf>
2023-09-17 22:12:22 -0700

    provider stubs to pass checks

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider aws {
-  alias = "default"
-}
-
-provider aws {
-  alias = "us_east_1"
-}
-
 locals {
   domain_name_short = split(".", var.domain_name)[0]
   ecr_registry_fqdn = "${var.aws_account_id}.dkr.ecr.${var.ecr_registry_region}.amazonaws.com"
---
+++ stubs/stub.provider.tf
@@ -0,0 +1,9 @@
+provider aws {
+  alias = "default"
+  region = "us-west-1"
+}
+
+provider aws {
+  alias = "us_east_1"
+  region = "us-east-1"
+}
